819b79cb40c8880b2bf5123f798161f17d71f58d
yasinskislava <yasinskislava@gmail.com>
2023-08-25 19:09:44 +0300

    hw22

---
+++ task-3.js
@@ -0,0 +1,30 @@
+const Storage = function ([...items]) {
+  this.items = items;
+  this.getItems = function () {
+    return this.items;
+  };
+
+  this.addItem = function (item) {
+    this.items.push(item);
+  };
+
+  this.removeItem = function (item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  };
+};
+
+const storage = new Storage([
+  "Нанітоіди",
+  "Пролонгер",
+  "Залізні жупи",
+  "Антигравітатор",
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem("Дроїд");
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
